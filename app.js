bc566788c84285addd9cca9ccfd6bdf39cda5df0
Derek Hudgins <hudgins.derek@gmail.com>
2021-03-24 14:27:47 -0700

    finished name tag with name gen

---
+++ app.js
@@ -0,0 +1,17 @@
+const nameInput = document.getElementById('name-input');
+const changeBtn = document.getElementById('change-btn');
+const greetingDisplay = document.getElementById('greeting-display');
+changeBtn.addEventListener('click', () => {
+    greetingDisplay.textContent = nameInput.value; 
+});
+
+const pinkbtn = document.getElementById('pink-btn');
+const lightgreenbtn = document.getElementById('light-green-btn');
+const lightbluebtn = document.getElementById('light-blue-btn');
+
+
+pinkbtn.addEventListener('click'), () => {
+    
+    
+}
+
